42581daa58dc37c565e0a3b2d078b50867d63a62
Paolo Sassi <paolo.sassi91@gmail.com>
2017-07-31 18:04:29 +0200

    added OM2M files

---
+++ Infrastructure/infrastructure/src/main/java/svm/infrastructure/App.java
@@ -0,0 +1,13 @@
+package svm.infrastructure;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
