ed70a5c988ea7da35e431c7700b66de209e72f3b
Gyro91 <rotundo.matteo@gmail.com>
2017-08-02 12:53:54 +0200

    Id put/get implemented

--- 6LowPan/sensors/food_vending_machine.c
+++ 6LowPan/sensors/food_vending_machine.c
@@ -1,9 +1,25 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "contiki.h"
 #include "contiki-net.h"
 #include "rest-engine.h"
 
+#define DEBUG 1
+#if DEBUG
+#define PRINTF(...) printf(__VA_ARGS__)
+#define PRINT6ADDR(addr) PRINTF("[%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x]", ((uint8_t *)addr)[0], ((uint8_t *)addr)[1], ((uint8_t *)addr)[2], ((uint8_t *)addr)[3], ((uint8_t *)addr)[4], ((uint8_t *)addr)[5], ((uint8_t *)addr)[6], ((uint8_t *)addr)[7], ((uint8_t *)addr)[8], ((uint8_t *)addr)[9], ((uint8_t *)addr)[10], ((uint8_t *)addr)[11], ((uint8_t *)addr)[12], ((uint8_t *)addr)[13], ((uint8_t *)addr)[14], ((uint8_t *)addr)[15])
+#define PRINTLLADDR(lladdr) PRINTF("[%02x:%02x:%02x:%02x:%02x:%02x]",(lladdr)->addr[0], (lladdr)->addr[1], (lladdr)->addr[2], (lladdr)->addr[3],(lladdr)->addr[4], (lladdr)->addr[5])
+#else
+#define PRINTF(...)
+#define PRINT6ADDR(addr)
+#define PRINTLLADDR(addr)
+#endif
 
-static void res_get_handler(void *request, void *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
+static int machine_id = 0;
+static void res_get_handler(void *request, void *response, 
+  uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
 
 RESOURCE(resource_example,
          "title=\"Hello world: ?len=0..\";rt=\"Text\"",
@@ -13,7 +29,8 @@ RESOURCE(resource_example,
          NULL);
 
 static void
-res_get_handler(void *request, void *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
+res_get_handler(void *request, void *response, uint8_t *buffer, 
+  uint16_t preferred_size, int32_t *offset)
 {
   char const *const message = "Hello World!";
   int length = 12; 
@@ -23,6 +40,45 @@ res_get_handler(void *request, void *response, uint8_t *buffer, uint16_t preferr
   REST.set_header_etag(response, (uint8_t *)&length, 1);
   REST.set_response_payload(response, buffer, length);
 }
+
+
+void id_get_handler(void* request, void* response, 
+  uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
+{
+  /* Populat the buffer with the response payload*/
+  char message[30];
+  int length = 30;
+
+  printf("Get\n");
+  sprintf(message, "{'id':'%d'}", machine_id);
+  length = strlen(message);
+  memcpy(buffer, message, length);
+
+  REST.set_header_content_type(response, REST.type.TEXT_PLAIN); 
+  REST.set_header_etag(response, (uint8_t *) &length, 1);
+  REST.set_response_payload(response, buffer, length);
+}
+
+void id_put_handler(void* request, void* response, 
+  uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
+{
+  int new_value, len;
+  const char *val = NULL;
+  
+  printf("Put\n");
+  len = REST.get_post_variable(request, "value", &val);
+     
+  if (len > 0){
+     new_value = atoi(val);
+     PRINTF("new value %u\n", new_value);
+     machine_id = new_value;
+     REST.set_response_status(response, REST.status.CREATED);
+  } else {
+     REST.set_response_status(response, REST.status.BAD_REQUEST);
+  }
+}
+
+RESOURCE(id, "title=\"Get/Put value=10\";rt=\"Text\"", id_get_handler, id_put_handler, id_put_handler, NULL);
 
 PROCESS(server, "CoAP Server");
 AUTOSTART_PROCESSES(&server);
@@ -36,7 +92,7 @@ PROCESS_THREAD(server, ev, data)
   rest_init_engine();
 
   rest_activate_resource(&resource_example, "example");
-
+  rest_activate_resource(&id, "machineId");
   while(1) {
     PROCESS_WAIT_EVENT();
   }
