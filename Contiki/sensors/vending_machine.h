e34c8d9d09d491aa31311f0feaf79c854d1c3b1a
Gyro91 <rotundo.matteo@gmail.com>
2017-08-24 11:34:35 +0200

    Implemented a time based resource variation

--- Contiki/sensors/vending_machine.h
+++ Contiki/sensors/vending_machine.h
@@ -1,6 +1,9 @@
 #define MAX_PRODUCT_AVAILABILITY 5
 #define PISA_LATITUDE 43
 #define PISA_LONGITUDE 10
+#define QTY_DELAY 30
+#define ALARM_DELAY 60
+#define STATUS_DELAY 120
 
 struct product {
 	uint8_t remaining_qty;
