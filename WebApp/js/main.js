73f957be5ceead90b63e2cccd346db4f216da1f0
Paolo Sassi <paolo.sassi91@gmail.com>
2017-09-02 18:31:12 +0200

    added write

--- WebApp/js/main.js
+++ WebApp/js/main.js
@@ -10,6 +10,7 @@
 
 
 /** Global variables */
+var page = "index";
 var socket;
 var socket_ok = false;
 var current_page;
@@ -18,8 +19,82 @@ var update_timer = null;
 var page_timer = null;
 
 /*===========================================================================*/
+/*=================== Startup and Cleaning Functions ========================*/
+/*===========================================================================*/
+
+/**
+ * This code is executed when the document is loaded.
+ */
+$(document).ready(function(){
+    
+    createIndexPage();
+    
+    /* Initialize the svm array */
+    svm = Array(0);
+    
+    $(".close").onclick = function() {
+        $().alert('close');
+    }
+    
+    $(".nav_link").not(".navbar-brand").click(function() {
+        if (!socket_ok && $(this).text() != "Help") {
+            showAlert("The WebSocket is not connected!", "warning");
+            return;
+        }     
+        switch($(this).text()) {
+            case "City Map":
+                createMapPage();
+                break;
+            case "Analytics":
+                createAnalyticsPage();
+                break;
+            case "Plan Route":
+                createRoutePage();
+                break;
+            case "Help":
+                createHelpPage();
+            default:
+                break;
+        }  
+    })
+    connectWS();
+});
+
+/**
+ * Closes the WebSocket before closing the window.
+ */
+$(window).on('beforeunload', function(){
+    if (socket.readyState === WebSocket.OPEN)
+        socket.close();
+});
+
+/*===========================================================================*/
 /*======================== WebSocket Functions ==============================*/
 /*===========================================================================*/
+
+/**
+ * Connect to the WebSocket Server (if possible).
+ */
+function connectWS() {
+    var host;
+    if (!("WebSocket" in window)) {
+        alert("Your browser doesn't support WebSockets, please " +
+                          "use Google Chrome or Mozilla Firefox");
+        return;
+    }
+    
+    try {
+        host = "ws://127.0.0.1:8000/";
+        socket = new WebSocket(host);
+        
+        socket.onopen = onOpenHandler;   
+        socket.onerror = onErrorHandler;
+        socket.onclose = onCloseHandler;
+        socket.onmessage = onMessageHandler;
+    } catch (exception) {
+        showAlert("Exception in WebSocket connection: " + exception, "danger");
+    }
+}
 
 /**
  * WebSocket onopen event handler.
@@ -114,14 +189,17 @@ function onMessageHandler(msg) {
                             switch (prod_res) {
                                 case "qty":
                                     svm[vm_index].products[prod_index][prod_res] =
-                                    prod_cnt[prod_res];
+                                        prod_cnt[prod_res];
                                     break;
                                 case "price":
                                     svm[vm_index].products[prod_index][prod_res] =
                                     prod_cnt[prod_res].toFixed(2);
                                 default:
                                     break;
-                            }                
+                            }
+                            svm[vm_index].updated.products = true;
+                            svm[vm_index].products[prod_index].
+                                updated[prod_res] = true;
                         }
                     }
                 } else {
@@ -143,33 +221,10 @@ function onMessageHandler(msg) {
                         default:
                             break;        
                     }
+                    svm[vm_index].updated[res] = true;
                 }
             }
         }
-    }
-}
-
-/**
- * Connect to the WebSocket Server (if possible).
- */
-function connectWS() {
-    var host;
-    if (!("WebSocket" in window)) {
-        alert("Your browser doesn't support WebSockets, please " +
-                          "use Google Chrome or Mozilla Firefox");
-        return;
-    }
-    
-    try {
-        host = "ws://127.0.0.1:8000/";
-        socket = new WebSocket(host);
-        
-        socket.onopen = onOpenHandler;   
-        socket.onerror = onErrorHandler;
-        socket.onclose = onCloseHandler;
-        socket.onmessage = onMessageHandler;
-    } catch (exception) {
-        showAlert("Exception in WebSocket connection: " + exception, "danger");
     }
 }
 
@@ -178,6 +233,28 @@ function connectWS() {
  */
 function requestUpdate() {
     socket.send(update_req);
+}
+
+/**
+ * Send an updated resource to the IN
+ *
+ */
+function sendUpdate(vm, res, value, prod) {
+    var msg;
+    prod = prod || "NO_PROD"; /* Default value */
+    
+    if (prod == "NO_PROD") {
+        /* Update a VM resource */
+        msg = {"type": "W", "id": vm.id, "resource": res,
+               "content": {}};
+    } else {
+        /* Update a product resource */
+        msg = {"type": "W", "id": vm.id, "resource":
+               prod.id + res, "content": {}};
+    }
+    msg.content[res] = value;
+    showAlert(JSON.stringify(msg));
+    socket.send(JSON.stringify(msg));
 }
 
 /*===========================================================================*/
@@ -249,37 +326,6 @@ function createMapPage() {
 }
 
 /**
- * Creates the Analytics page contents.
- */
-function createAnalyticsPage() {
-    var i = 1;
-    current_page = "analytics";
-    
-    /* Start the page refresh timer */
-    //if (page_timer == null)
-    //    page_timer = setInterval(createAnalyticsPage, 1000);
-        
-    /* Empty the old content */
-    $("#main_cont").empty();
-    $(".page-header").empty();
-    $(".nav_link").attr("class", "nav_link");
-    $(".nav_link").first().attr("class", "navbar-brand nav_link");
-    
-    /* Add the new content */
-    $(".page-header").append("Analytics");
-    $(".nav_link").eq(3).attr("class", "nav_link active");
-    $("#main_cont").append('<div class="row"></div>');
-    for (vm in svm) {
-        $("#main_cont").children(".row").last().append(getSVMPanel(vm));
-        if (i % 3 == 0) {
-            /* Add a row */
-            $("#main_cont").append('<div class="row"></div>');
-        }
-        i++;
-    }
-}
-
-/**
  * Creates the Plan Route page contents.
  */
 function createRoutePage() {
@@ -320,8 +366,326 @@ function createHelpPage() {
     $(".page-header").append("Help");
 }
 
+/*===========================================================================*/
+/*========================== Analytics Functions ============================*/
+/*===========================================================================*/
+
 /**
- * Shows non-invasive alert.
+ * Creates the Analytics page contents.
+ */
+function createAnalyticsPage() {
+    var i = 1;
+    current_page = "analytics";
+    
+    /* Start the page refresh timer */
+    if (page_timer == null)
+        page_timer = setInterval(checkUpdates, 1000);
+        
+    /* Empty the old content */
+    $("#main_cont").empty();
+    $(".page-header").empty();
+    $(".nav_link").attr("class", "nav_link");
+    $(".nav_link").first().attr("class", "navbar-brand nav_link");
+    
+    /* Add the new content */
+    $(".page-header").append("Analytics");
+    $(".nav_link").eq(3).attr("class", "nav_link active");
+    $("#main_cont").append('<div class="row"></div>');
+    for (vm in svm) {
+        $("#main_cont").children(".row").last().append(getSVMPanel(vm));
+        if (i % 3 == 0) {
+            /* Add a row */
+            $("#main_cont").append('<div class="row"></div>');
+        }
+        i++;
+    }
+    
+    $(".svm-edit-res").mouseenter(showEditIcon);
+    $(".svm-edit-res").mouseleave(hideEditIcon);
+}
+
+/**
+ * Checks if any resource value has changed
+ */
+function checkUpdates() {
+    var is_updated = false;
+    var value;
+    
+    for (vm in svm) {
+        for (res in svm[vm].updated) {
+            if (svm[vm].updated[res]) {
+                /* This resource has been modified and its displayed
+                 * value must be updated. */
+                if (res == "products") {
+                    /* Check which products have been modified */
+                    for (prod in svm[vm].products) {
+                        for (prod_res in svm[vm].products[prod].updated) {
+                            is_updated = svm[vm].products[prod].updated[prod_res];
+                            if (is_updated) {
+                                svm[vm].products[prod].updated[prod_res] = false;
+                                changeDispValue(svm[vm], prod_res,
+                                                svm[vm].products[prod][prod_res],
+                                                prod);
+                            }
+                        }
+                    }
+                } else {
+                    /* Check which VM resource has been modified */
+                    switch (res) {
+                        case "status":
+                        case "alarm":
+                            value = svm[vm][res].toStr();
+                            break;
+                        case "pos":
+                            value = svm[vm][res].lat + ", " + svm[vm][res].lng;
+                            break;
+                        default:
+                            value = svm[vm][res];
+                            break;
+                    }
+                    svm[vm].updated[res] = false;
+                    changeDispValue(svm[vm], res, value);
+                }
+            }
+        }
+        /* All products have been updated */
+        svm[vm].updated.products = false;
+    }
+}
+
+function changeDispValue(vm, res, value, prod) {
+    var id;
+    prod = prod || "NO_PROD"; /* default value */
+    
+    if (prod != "NO_PROD") {
+        id = "#" + vm.id + "-" + prod.id + "-" + res;
+    } else {
+        id = "#" + vm.id + "-" + res;
+    }
+    
+    $(id).empty();
+    $(id).text(value);
+    //$(id).mouseenter(showEditIcon);
+    //$(id).mouseleave(hideEditIcon);
+}
+
+/**
+ * Get the HTML code to show the selected VM (and its products) in a panel.
+ * @param {STRING} index Index of the VM on the svm array
+ * @returns {STRING} The HTML code to display the panel
+ */
+function getSVMPanel(index) {
+    var ret = "";
+    var event = "onmouseover";
+    var prods = svm[index].products;
+    var offset = (index % 2 == 0)? "col-md-offset-1" : "col-md-offset-2";
+    
+    ret = ret + '<div class="panel panel-primary svm-panel col-md-4 ' + offset +
+        '"><div class="panel-heading"><h3 class="panel-title">' + svm[index].id +
+        '</h3></div><div class="panel-body"><h4>General Info</h4>' +
+        addVMRes(svm[index], "status") + addVMRes(svm[index], "pos") +
+        addVMRes(svm[index], "tempsens") + addVMRes(svm[index], "tempdes") +
+        addVMRes(svm[index], "alarm") + "<br><h4>Products</h4>";
+    for (prod in prods) {
+        ret = ret + '<div class="panel panel-default"><div class="panel-' +
+            'heading"><strong>' + prods[prod].id + '</strong></div><div class="panel-body">' +
+            addProdRes(svm[index], prods[prod], "qty") +
+            addProdRes(svm[index], prods[prod], "price") + '</div></div>';
+    }
+    ret = ret + '</div></div>';
+    return ret;
+}
+
+/**
+ * Adds the HTML code needed for displaying a resource of a specific VM.
+ * @param {VendingMachine} vm The VM who owns the resources
+ * @param {STRING} res The name of the resource
+ * @returns {STRING} The HTML code
+ */
+function addVMRes(vm, res) {
+    var ret = "";
+    var value;
+    
+    switch (res) {
+        case "status":
+            ret += '<strong>Status: </strong><span class="svm-edit-res" id="';
+            value = vm.status.toStr();
+            break;
+        case "pos":
+            ret += '<strong>Position: </strong><span class="svm-edit-res" id="';
+            value = vm.pos.lat + ", " + vm.pos.lng;
+            break;
+        case "tempsens":
+            ret += '<strong>Sensed Temp.: </strong><span id="';
+            value = vm.tempsens;
+            break;
+        case "tempdes":
+            ret += '<strong>Desidered Temp.: </strong><span ' +
+                'class="svm-edit-res" id="';
+            value = vm.tempdes;
+            break;
+        case "alarm":
+            ret += '<strong>Alarm: </strong><span class="svm-edit-res" id="';
+            value = vm.alarm.toStr();
+            break;
+        default:
+            return ret;
+            break;
+    }  
+    ret += vm.id.toLowerCase() + "-" + res + '">' + value + '</span><br>';
+    return ret;
+}
+
+/**
+ * Adds the HTML code needed for displaying a resource of a specific product.
+ * @param {VendingMachine} vm The VM who owns the resources
+ * @param {STRING} res The name of the resource
+ * @returns {STRING} The HTML code
+ */
+function addProdRes(vm, prod, res) {
+    var ret = "";
+    var value;
+    
+    switch (res) {
+        case "qty":
+            ret += '<strong>Quantity: </strong><span class="svm-edit-res" id="';
+            break;
+        case "price":
+            ret += '<strong>Price: </strong><span class="svm-edit-res" id="';
+            break;
+        default:
+            return ret;
+            break;
+    }
+    value = prod[res];
+    ret += vm.id.toLowerCase() + "-" + prod.id + "-" + res + '">' + value +
+        '</span><br>';
+    return ret;
+}
+
+/**
+ * Shows the edit icon next to the text.
+ */
+function showEditIcon() {
+    $(this).append('<span class="glyphicon glyphicon-wrench edit"' +
+                   'aria-hidden="true"></span>');
+    $(this).children().last().click(editRes);
+}
+
+/**
+ * Hides the edit icon next to the text.
+ */
+function hideEditIcon() {
+    $(this).children().last().remove();
+}
+
+/**
+ * Edits the selected resource.
+ * @param {OBJECT} obj The DOM object who fired the event.
+ */
+function editRes() {
+    var id = $(this).parent().attr("id");
+    var type = id.split("-");
+    type = type[type.length - 1];
+    /* Store and remove the resource value */
+    var value = $(this).parent().text();
+    $(this).parent().text("");
+    /* Add a form specific for the res type */
+    switch (type) {
+        case "status":
+            $("#" + id).append('<select><option value=0>OFF</option>' +
+                               '<option value=1>ON</option></select>');
+            break;
+        case "alarm":
+            $("#" + id).append('<select><option value="N">No alarm</option>' +
+                               '<option value="I">Intrusion</option>' +
+                               '<option value="F">Fault</option></select>');
+            break;
+        case "pos":
+             $("#" + id).append('<input type="text" value="' + value + '">' +
+                               '</input>');
+            break;
+        case "tempdes":
+        case "qty":
+        case "price":
+            $("#" + id).append('<input class="short-input" type="text"' +
+                               'value="' + value + '"></input>');
+            break;
+        default:
+            return;
+    }
+    
+    $("#" + id).append('<span class="glyphicon glyphicon-ok edit"' +
+                   'aria-hidden="true"></span>');
+    $("#" + id).children().last().click(confirmEdit);
+    $("#" + id).append('<span class="glyphicon glyphicon-remove edit"' +
+                   'aria-hidden="true"></span>');
+    $("#" + id).children().last().click(cancelEdit);
+    $("#" + id).unbind("mouseenter", showEditIcon);
+    $("#" + id).unbind("mouseleave", hideEditIcon);
+}
+
+function confirmEdit() {
+    var id = $(this).parent().attr("id");
+    var type = id.split("-");
+    var index = findVM(id.split("-")[0].toUpperCase());
+    var value =  $("#" + id).children().first().val();
+    if (type.length > 2) {
+        /* A product resource has been modified */
+        var prod = findProduct(index, type[1]);
+        type = type[2];
+        sendUpdate(svm[index], type, value, svm[index].products[prod]);
+    } else {
+        /* A VM resource has been modified */
+        type = type[1];
+        sendUpdate(svm[index], type, value);
+    }
+    
+}
+
+function cancelEdit() {
+    var id = $(this).parent().attr("id");
+    var type = id.split("-");
+    var index = findVM(id.split("-")[0].toUpperCase());
+    var value;
+    
+    if (type.length > 2) {
+        /* A product resource has been modified */
+        var prod = findProduct(index, type[1]);
+        type = type[2];
+    } else {
+        /* A VM resource has been modified */
+        type = type[1];
+    }
+    
+    switch (type) {
+        case "status":
+        case "alarm":
+            value = svm[index][type].toStr();
+            break;
+        case "pos":
+            value = svm[index][type].lat + ", " + svm[index][type].lng;
+            break;
+        case "qty":
+        case "price":
+            value = svm[index].products[prod][type];
+            break;
+        default:
+            value = svm[index][type];
+            break;
+    }
+    $("#" + id).empty();
+    $("#" + id).text(value);
+    $("#" + id).mouseenter(showEditIcon);
+    $("#" + id).mouseleave(hideEditIcon);
+}
+
+/*===========================================================================*/
+/*============================ Utility Functions ============================*/
+/*===========================================================================*/
+
+/**
+ * Shows a non-invasive alert.
  * @param {STRING} msg Message to be shown
  * @param {STRING} type Type of the alert (optional). It can be one of:
  *                 'success', 'info', 'warning' and 'danger'
@@ -336,54 +700,3 @@ function showAlert(msg, type) {
                        '"Close"><span aria-hidden="true">&times;</span>' +
                        '</button>' + msg + '</div>');
 }
-
-/*===========================================================================*/
-/*=================== Startup and Cleaning Functions ========================*/
-/*===========================================================================*/
-
-/**
- * This code is executed when the document is loaded.
- */
-$(document).ready(function(){
-    
-    createIndexPage();
-    
-    /* Initialize the svm array */
-    svm = Array(0);
-    
-    $(".close").onclick = function() {
-        $().alert('close');
-    }
-    
-    $(".nav_link").not(".navbar-brand").click(function() {
-        if (!socket_ok && $(this).text() != "Help") {
-            showAlert("The WebSocket is not connected!", "warning");
-            return;
-        }
-        
-        switch($(this).text()) {
-            case "City Map":
-                createMapPage();
-                break;
-            case "Analytics":
-                createAnalyticsPage();
-                break;
-            case "Plan Route":
-                createRoutePage();
-                break;
-            case "Help":
-                createHelpPage();
-            default:
-                break;
-        }  
-    })
-    connectWS();
-});
-
-/**
- * Closes the WebSocket before closing the window.
- */
-$(window).on('beforeunload', function(){
-    if (socket.readyState === WebSocket.OPEN)
-        socket.close();
-});
